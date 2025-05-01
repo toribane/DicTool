e9346011ab0ce131a41596e822cef60e122c4aea
Katsuhiko Toba <katsuhiko.toba@outlook.jp>
2025-05-02 03:28:55 +0900

    get emoji-test.txt

--- data/get-files.bat
+++ data/get-files.bat
@@ -13,8 +13,10 @@ curl -O https://raw.githubusercontent.com/google/mozc/refs/heads/master/src/data
 curl -O https://raw.githubusercontent.com/google/mozc/refs/heads/master/src/data/dictionary_oss/id.def
 
 curl -O https://raw.githubusercontent.com/mtripg6666tdr/Kaomoji_proj/refs/heads/master/src/kaomoji.txt
+
 curl -O https://raw.githubusercontent.com/unicode-org/cldr/refs/heads/main/common/transforms/Japn-Latn.xml
 curl -O https://raw.githubusercontent.com/unicode-org/cldr/refs/heads/main/common/annotations/ja.xml
+curl -O https://raw.githubusercontent.com/unicode-org/unicodetools/refs/heads/main/unicodetools/data/emoji/dev/emoji-test.txt
 
 curl -O https://raw.githubusercontent.com/skk-dev/dict/refs/heads/master/SKK-JISYO.L
 curl -O https://raw.githubusercontent.com/skk-dev/dict/refs/heads/master/SKK-JISYO.geo
